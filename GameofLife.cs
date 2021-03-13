8b68ce2565ad4778d0a49c3e927bf32a2c13ccc8
Cpt. Squirrel <stelland1234@gmail.com>
2021-03-13 19:18:34 +0100

    Cleaned up. Added camera controls + zoom. Todo: Fix camera multithreading

--- GameofLife.cs
+++ GameofLife.cs
@@ -11,15 +11,17 @@ namespace Conway
     public class GameofLife : Game
     {
         private int GridSize; //The size of the grid
-        private int _cellScale; //How large the Cell's are
-        private Texture2D _aliveCellTexture; //The texture Cell's render with
+        public int CellScale; //How large the Cell's are
+        private Texture2D CellTexure; //The texture Cell's render with
         public static List<Cell> AliveCells; //Buffer of all alive Cell's
         public static List<Cell> SpawnBuffer; //Cell's awaiting to be created
         public static List<Cell> KillBuffer; //Cell's to be terminated
-        public static List<(int, int)> CheckedDeadCells;
-        private static (int, int) lastSummonedCell = (0, 0);
+        public static List<(int, int)> CheckedDeadCells; //Preventing checking same cell multiple times
+        //private static (int, int) lastSummonedCell = (0, 0); //For debugging
         List<(int, int)> _blacklistedCells = new List<(int, int)>(); //List of already checked cells; For use in resurrecting
 
+        private static Vector2 CameraPosition = new Vector2(0, 0);
+        private int _oldMouseWheelValue = Mouse.GetState().ScrollWheelValue;
 
         private Stopwatch DeltaTime = new Stopwatch();
         private Random _random = new Random();
@@ -86,12 +88,16 @@ namespace Conway
 
         protected override void Initialize()
         {
-            _cellScale = 6;
+            CellScale = 5;
             GridSize = 100;
             AliveCells = new List<Cell>();
             SpawnBuffer = new List<Cell>();
             KillBuffer = new List<Cell>();
             CheckedDeadCells = new List<(int, int)> ();
+
+            //Initialize and start cameraThread
+            Thread CameraThread = new Thread(cameraThread);
+            CameraThread.Start();
 
             //Create the 'aircraft carrier'
             /*AliveCells.Add(new Cell( (5, 5) ) );
@@ -103,22 +109,27 @@ namespace Conway
             AliveCells.Add(new Cell( (6, 6) ) );*/
 
             //Create a replicator
-            AliveCells.Add(new Cell( (3, 3) ) );
+            /*AliveCells.Add(new Cell( (3, 3) ) );
             AliveCells.Add(new Cell( (4, 3) ) );
             AliveCells.Add(new Cell( (5, 3) ) );
             AliveCells.Add(new Cell( (3, 4) ) );
             AliveCells.Add(new Cell( (5, 4) ) );
             AliveCells.Add(new Cell( (3, 5) ) );
             AliveCells.Add(new Cell( (4, 5) ) );
-            AliveCells.Add(new Cell( (5, 5) ) );
+            AliveCells.Add(new Cell( (5, 5) ) );*/
+
+            //Create a replicator
+            AliveCells.Add(new Cell( (100, 100) ) );
+            AliveCells.Add(new Cell( (103, 100) ) );
+            AliveCells.Add(new Cell( (100, 101) ) );
+            AliveCells.Add(new Cell( (101, 101) ) );
+            AliveCells.Add(new Cell( (102, 101) ) );
+            AliveCells.Add(new Cell( (101, 102) ) );
 
             _graphics.PreferredBackBufferWidth = 1280;
             _graphics.PreferredBackBufferHeight = 920;
             _graphics.ApplyChanges();
             IsMouseVisible = true;
-            /*IsFixedTimeStep = true;
-            TargetElapsedTime = TimeSpan.FromSeconds(1);
-            _graphics.SynchronizeWithVerticalRetrace = false;*/
 
             base.Initialize();
         }
@@ -126,22 +137,14 @@ namespace Conway
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            /* \/Create the cell texture\/ */
-            _aliveCellTexture = new Texture2D(GraphicsDevice, _cellScale, _cellScale); //Declare texture
-            Color[] _textureColor = new Color[_cellScale * _cellScale]; //New Color[] to fill texture with
-            for (int _index = 0; _index < _textureColor.Length; _index++) //Populate _textureColor
-                { _textureColor[_index] = Color.Black; }
-            for (int _index = 0; _index < _cellScale * _cellScale; _index++) { //Fill texture with _textureColor
-                _aliveCellTexture.SetData<Color>(_textureColor);
-            }
+            refreshTextures();
         }
 
         protected override void Update(GameTime gameTime)
         {
             //Spawn cells when spacebar is held
             if (Keyboard.GetState().IsKeyDown(Keys.Space)) {
-                for (int _index = 0; _index < 500; _index++)
+                for (int _index = 0; _index < 600; _index++)
                 {
                     (int, int) _newCoords;
                     do {
@@ -150,7 +153,7 @@ namespace Conway
                     AliveCells.Add(new Cell(_newCoords));
                 }
             }
-
+            
             //Check simulation if enough time has passed
             DeltaTime.Start(); //Make sure timer is started
             if (DeltaTime.Elapsed > TimeSpan.FromMilliseconds(20))
@@ -183,53 +186,32 @@ namespace Conway
                 }
 
                 //Check which neighbours should be resurrected (if there are any)
-                //TODO: Check more than one cell per thread
                 if (AliveCells.Count > 0)
                 {
-                    //List<(int, int)> _blackListedCells = new List<(int, int)>(); //Don't check already known cells
                     _blacklistedCells.Clear(); //Clear the blacklisted cells before beginning
                     List<Thread> _concurrentThreads = new List<Thread>();
-                    for (int _index = 0; _index < AliveCells.Count - 1; _index++) //Check each cell to have neighbours resurrected
+                    int _threads = (int)Math.Ceiling((double)AliveCells.Count / 100);
+                    for (int _index = 0; _index < _threads; _index++) //Check each cell to have neighbours resurrected
                     {
-                        _concurrentThreads.Add(new Thread(() => resurrectMethod(AliveCells[_index])));
+                        int _endRange;
+                        int _startRange = _index * 100;
+                        if (_index * 100 + 99 > AliveCells.Count) _endRange = AliveCells.Count;
+                        else _endRange = _index * 100 + 99;
+                        Cell[] _cellArray = new Cell[_endRange - _startRange];
+                        for (int _field = 0; _field < _cellArray.Length; _field++)
+                            _cellArray[_field] = AliveCells[_index * 100 + _field];
+                        _concurrentThreads.Add(new Thread(() => resurrectMethod(_cellArray)));
                         _concurrentThreads[_index].Start();
                     }
                     foreach (Thread _thread in _concurrentThreads)
                         _thread.Join(); //Wait for all threads to finalize
-
-                    /*{
-                        (int, int)[] _neighbours = new (int, int)[8]; //Save this cells neighbouring positions
-                        { //Add each grid to check
-                            _neighbours[0] = (_cell._position.Item1 - 1, _cell._position.Item2 - 1);
-                            _neighbours[1] = (_cell._position.Item1 + 0, _cell._position.Item2 - 1);
-                            _neighbours[2] = (_cell._position.Item1 + 1, _cell._position.Item2 - 1);
-                            _neighbours[3] = (_cell._position.Item1 - 1, _cell._position.Item2 + 0);
-                            _neighbours[4] = (_cell._position.Item1 + 1, _cell._position.Item2 + 0);
-                            _neighbours[5] = (_cell._position.Item1 - 1, _cell._position.Item2 + 1);
-                            _neighbours[6] = (_cell._position.Item1 + 0, _cell._position.Item2 + 1);
-                            _neighbours[7] = (_cell._position.Item1 + 1, _cell._position.Item2 + 1);
-                        }
-                        foreach ((int, int) _neighbourCell in _neighbours) { //For each neighbour...:
-                            if (isAlive(_neighbourCell) != null) break; //Skip if the cell is already alive
-                            foreach ((int, int) _blackListedCell in _blacklistedCells) { //Check if this cell has already been checked
-                                if (_neighbourCell == _blackListedCell) goto Post_Resurrect; //If so, skip
-                        }                                                                   //Else:
-                            if (GridNeighbours(_neighbourCell).Length == 3) { //Does it have 3 neighbours?
-                                SpawnBuffer.Add(new Cell(_neighbourCell));
-                                if (lastSummonedCell == _neighbourCell) Console.Write("DUPLICATE SPAWN! ");
-                                lastSummonedCell = _neighbourCell;
-                                Console.WriteLine("Summoned Cell at {0}", _neighbourCell);
-                            }
-                        }
-                        Post_Resurrect: { /* Goto here if this Cell has been checked to skip / }
-                    }*/
 
                 }
     
                 //If _spawn || _kill buffers != empty then
                 //TODO: Make it not start a thread when under threshold
                 if (SpawnBuffer.Count != 0)
-                {
+                /*{
                     List<Thread> _concurrentThreads = new List<Thread>();
                     int _threads = (int)Math.Ceiling((double)SpawnBuffer.Count / 400);
                     for (int _index = 0; _index < _threads; _index++)
@@ -240,20 +222,20 @@ namespace Conway
                     foreach (Thread _thread in _concurrentThreads)
                         _thread.Join(); //Wait for all threads to finalize
                     SpawnBuffer.Clear(); //Clear buffer
-                }
-                /*{
+                }*/
+                {
                     foreach (Cell _cell in SpawnBuffer)
                     {
                         AliveCells.Add(_cell);
                     } SpawnBuffer.Clear(); //Clear buffer
-                }*/
+                }
 
                 if (KillBuffer.Count != 0)
                 {
                     foreach (Cell _cell in KillBuffer)
                     {
                         if (!AliveCells.Remove(_cell)) Console.WriteLine("FAILED TO KILL CELL {0}!", _cell._position);
-                        Console.WriteLine("Killed cell at {0}", _cell._position);
+                        //Console.WriteLine("Killed cell at {0}", _cell._position);
                     } KillBuffer.Clear(); //Clear buffer
                 }
                 //End of update block
@@ -262,29 +244,33 @@ namespace Conway
             base.Update(gameTime);
         }
 
-        void resurrectMethod(Cell cell)
+        void resurrectMethod(Cell[] cellArray)
         {
-            (int, int)[] _neighbours = new (int, int)[8]; //Save this cells neighbouring positions
-            { //Add each grid to check
-                _neighbours[0] = (cell._position.Item1 - 1, cell._position.Item2 - 1);
-                _neighbours[1] = (cell._position.Item1 + 0, cell._position.Item2 - 1);
-                _neighbours[2] = (cell._position.Item1 + 1, cell._position.Item2 - 1);
-                _neighbours[3] = (cell._position.Item1 - 1, cell._position.Item2 + 0);
-                _neighbours[4] = (cell._position.Item1 + 1, cell._position.Item2 + 0);
-                _neighbours[5] = (cell._position.Item1 - 1, cell._position.Item2 + 1);
-                _neighbours[6] = (cell._position.Item1 + 0, cell._position.Item2 + 1);
-                _neighbours[7] = (cell._position.Item1 + 1, cell._position.Item2 + 1);
-            }
-            foreach ((int, int) _neighbourCell in _neighbours) { //For each neighbour...:
-                if (isAlive(_neighbourCell) != null) break; //Skip if the cell is already alive
-                for (int _index = 0; _index < _blacklistedCells.Count; _index++) { //Check if this cell has already been checked
-                    if (_neighbourCell == _blacklistedCells[_index]) return; //If so, skip
-                }                                                   //Else:
-                if (GridNeighbours(_neighbourCell).Length == 3) { //Does it have 3 neighbours?
-                    SpawnBuffer.Add(new Cell(_neighbourCell));
-                    //Console.WriteLine("Summoned Cell at {0}", _neighbourCell);
+            foreach (Cell cell in cellArray)
+            {
+                (int, int)[] _neighbours = new (int, int)[8]; //Save this cells neighbouring positions
+                { //Add each grid to check
+                    _neighbours[0] = (cell._position.Item1 - 1, cell._position.Item2 - 1);
+                    _neighbours[1] = (cell._position.Item1 + 0, cell._position.Item2 - 1);
+                    _neighbours[2] = (cell._position.Item1 + 1, cell._position.Item2 - 1);
+                    _neighbours[3] = (cell._position.Item1 - 1, cell._position.Item2 + 0);
+                    _neighbours[4] = (cell._position.Item1 + 1, cell._position.Item2 + 0);
+                    _neighbours[5] = (cell._position.Item1 - 1, cell._position.Item2 + 1);
+                    _neighbours[6] = (cell._position.Item1 + 0, cell._position.Item2 + 1);
+                    _neighbours[7] = (cell._position.Item1 + 1, cell._position.Item2 + 1);
                 }
-                _blacklistedCells.Add(_neighbourCell);
+                foreach ((int, int) _neighbourCell in _neighbours) { //For each neighbour...:
+                    if (isAlive(_neighbourCell) != null) continue; //Skip if the cell is already alive
+                    bool _isChecked = false;
+                    for (int _index = 0; _index < _blacklistedCells.Count; _index++) { //Check if this cell has already been checked
+                        if (_neighbourCell == _blacklistedCells[_index]) _isChecked = true; break; //If so, skip
+                    } if (_isChecked) continue;                                                   //Else:
+                    if (GridNeighbours(_neighbourCell).Length == 3) { //Does it have 3 neighbours?
+                        SpawnBuffer.Add(new Cell(_neighbourCell));
+                        //Console.WriteLine("Summoned Cell at {0}", _neighbourCell);
+                    }
+                    _blacklistedCells.Add(_neighbourCell);
+                }
             }
         }
 
@@ -297,6 +283,47 @@ namespace Conway
             }
         }
 
+        void refreshTextures() //Make new textures using the scales
+        {
+            CellTexure = new Texture2D(GraphicsDevice, CellScale, CellScale); //Declare texture
+            Color[] _textureColor = new Color[CellScale * CellScale]; //New Color[] to fill texture with
+            for (int _index = 0; _index < _textureColor.Length; _index++) //Populate _textureColor
+                { _textureColor[_index] = Color.Black; }
+            for (int _index = 0; _index < CellScale * CellScale; _index++) { //Fill texture with _textureColor
+                CellTexure.SetData<Color>(_textureColor);
+            }
+        }
+
+        void cameraThread()
+        {
+            Stopwatch _stopWatch = new Stopwatch();
+            _stopWatch.Start();
+            while (true)
+            {
+                CameraPosition.X += 1 * _stopWatch.ElapsedMilliseconds;
+                if (Keyboard.GetState().IsKeyDown(Keys.D)) CameraPosition.X -= 1 * _stopWatch.ElapsedMilliseconds;
+                if (Keyboard.GetState().IsKeyDown(Keys.W)) CameraPosition.Y += 1 * _stopWatch.ElapsedMilliseconds;
+                if (Keyboard.GetState().IsKeyDown(Keys.S)) CameraPosition.Y -= 1 * _stopWatch.ElapsedMilliseconds;
+                if (Mouse.GetState().ScrollWheelValue > _oldMouseWheelValue && CellScale < 10) //If the wheel has increased
+                {
+                    CellScale++;
+                    //Math.Clamp(CellScale, 1, 10); //Make sure it does not enter extremes
+                    refreshTextures();
+                }
+                else if (Mouse.GetState().ScrollWheelValue < _oldMouseWheelValue && CellScale > 1) //If the wheel has decreased
+                {
+                    CellScale--;
+                    //Math.Clamp(CellScale, 1, 10); //Make sure it does not enter extremes
+                    refreshTextures();
+                }
+                _oldMouseWheelValue = Mouse.GetState().ScrollWheelValue; //Set new _old value for mouse wheel
+
+                //Console.WriteLine(Mouse.GetState().ScrollWheelValue);
+                //Console.WriteLine(CellScale);
+                _stopWatch.Reset();
+            }
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Wheat);
@@ -304,8 +331,8 @@ namespace Conway
             try {
             foreach (Cell element in AliveCells)
             {
-                Vector2 _position = new Vector2(element._position.Item1*_cellScale, element._position.Item2*_cellScale);
-                _spriteBatch.Draw(_aliveCellTexture, _position, Color.White);
+                Vector2 _position = new Vector2(element._position.Item1*CellScale, element._position.Item2*CellScale);
+                _spriteBatch.Draw(CellTexure, _position + CameraPosition, Color.White);
             } } catch {}
             _spriteBatch.End();
 
